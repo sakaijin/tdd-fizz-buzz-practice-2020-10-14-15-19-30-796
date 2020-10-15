1c84eb1ba39ed1e80c8572d01a81151ebe5f6864
Erik <erik.daniel.nobleta@oocl.com>
2020-10-15 14:07:47 +0800

    [FizzBuzz][Erik-1]feature: return fizz when multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,9 @@ import static java.lang.String.valueOf;
 
 public class FizzBuzz {
     public String countOff(int count) {
+        if (count % 3 == 0 ){
+            return "Fizz";
+        }
         return valueOf(count);
     }
 }
