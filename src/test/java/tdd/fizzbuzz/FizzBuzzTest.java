411a3eb1d5b3ed8b5b8f339e45516cacfce11056
Erik <erik.daniel.nobleta@oocl.com>
2020-10-15 13:58:30 +0800

    [FizzBuzz][Erik-1]test: create test should count off be 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_fizzBuzz_when_count_off_given_1() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actual = fizzBuzz.countOff(1);
+        assertEquals("1", actual);
+    }
 }
