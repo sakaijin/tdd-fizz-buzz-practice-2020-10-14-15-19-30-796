b63535a8bc84b621290b9450ad43917c67f036bd
Erik <erik.daniel.nobleta@oocl.com>
2020-10-15 14:14:59 +0800

    [FizzBuzz][Erik-1]refactor: added constants for 5 and Buzz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -6,13 +6,15 @@ public class FizzBuzz {
 
     private static final String FIZZ = "Fizz";
     private static final int THREE = 3;
+    private static final int FIVE = 5;
+    private static final String BUZZ = "Buzz";
 
     public String countOff(int count) {
         if (count % THREE == 0 ){
             return FIZZ;
         }
-        if (count % 5 == 0){
-            return "Buzz";
+        if (count % FIVE == 0){
+            return BUZZ;
         }
         return valueOf(count);
     }
