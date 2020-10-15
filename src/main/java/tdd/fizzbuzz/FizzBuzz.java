17e852eda292f9a25bb652f627b7e36ea7fa7d9f
Erik <erik.daniel.nobleta@oocl.com>
2020-10-15 14:09:37 +0800

    [FizzBuzz][Erik-1]refactor: added constants for string Fizz and int 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,9 +3,13 @@ package tdd.fizzbuzz;
 import static java.lang.String.valueOf;
 
 public class FizzBuzz {
+
+    private static final String FIZZ = "Fizz";
+    private static final int THREE = 3;
+
     public String countOff(int count) {
-        if (count % 3 == 0 ){
-            return "Fizz";
+        if (count %  THREE == 0 ){
+            return FIZZ;
         }
         return valueOf(count);
     }
